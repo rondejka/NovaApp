13a6bf62edb24e3189906ff3681416062fa6d9b3
rondejka <76867691+rondejka@users.noreply.github.com>
2025-12-01 13:55:52 +0100

    Add files via upload

---
+++ api/entities.js
@@ -0,0 +1,15 @@
+import { base44 } from './base44Client';
+
+
+export const Task = base44.entities.Task;
+
+export const Robot = base44.entities.Robot;
+
+export const WellbeingEntry = base44.entities.WellbeingEntry;
+
+export const BloomStep = base44.entities.BloomStep;
+
+
+
+// auth sdk:
+export const User = base44.auth;
